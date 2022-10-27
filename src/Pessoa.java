6d7f3bfc5c4cbcc74d23b72dd89e4adeb094c284
Aluno <aluno@IPIRANGA.FATEC>
2022-10-27 16:04:13 -0300

    apostila01(oracle_ret_api): fim da secao 2.8

---
+++ src/Pessoa.java
@@ -0,0 +1,19 @@
+import lombok.Getter;
+import lombok.Setter;
+
+//javabean
+//pojo:plain old java object
+@Getter
+@Setter
+public class Pessoa {
+   private int codigo;
+   private String nome;
+   private int idade;
+   private String hobby; 
+
+   //public static void main(String[] args){
+   // Pessoa p = new Pessoa();
+   // p.setNome("Ana");
+   //System.out.println(p.getNome());
+   //}
+}
